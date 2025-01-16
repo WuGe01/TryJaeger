bd1e3b0881938cdb8cf6d704ca79d367c05db9ff
EG_吳宗融(Dennis_Wu) <dennis_wu@everglory.asia>
2025-01-16 17:27:32 +0800

    fix.測試的資料拿除

--- app/Http/Controllers/TraceTestController.php
+++ app/Http/Controllers/TraceTestController.php
@@ -15,22 +15,6 @@ class TraceTestController extends Controller
 
     public function test()
     {
-        $span = $this->tracer->startSpan('test-span');
-
-        // 設置標籤
-        $span->setTag('endpoint', 'index');
-        $span->log(['event' => 'start-span']);
-
-        // 模擬業務邏輯
-        sleep(1);
-
-        $span->log(['event' => 'end-span']);
-        $span->finish();
-
-        // 刷新數據
-        $this->tracer->flush();
-
-        // 返回測試 JSON 數據
         return response()->json([
             'message' => 'Test trace completed',
         ]);
