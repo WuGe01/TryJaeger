48e9af0c32fb9cf7662af80979d31f8b907186fa
EG_吳宗融(Dennis_Wu) <dennis_wu@everglory.asia>
2025-01-20 15:20:51 +0800

    fix.調整

--- app/Http/Middleware/JaegerMiddleware.php
+++ app/Http/Middleware/JaegerMiddleware.php
@@ -24,19 +24,20 @@ class JaegerMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // 創建 Span
-        $span = $this->tracer->startSpan($request->method() . ' ' . $request->path());
-        $span->setTag('http.method', $request->method());
-        $span->setTag('http.url', $request->fullUrl());
-
         $response = $next($request);
-
-        // 設置返回代碼
-        $span->setTag('http.status_code', $response->getStatusCode());
-        $span->finish();
-
-        // 刷新追蹤數據
-        $this->tracer->flush();
+        if ( $response->getStatusCode() != 200) {
+            // 創建 Span
+            $span = $this->tracer->startSpan($request->method() . ' ' . $request->path());
+            $span->setTag('http.method', $request->method());
+            $span->setTag('http.url', $request->fullUrl());
+    
+            // 設置返回代碼
+            $span->setTag('http.status_code', $response->getStatusCode());
+            $span->finish();
+    
+            // 刷新追蹤數據
+            $this->tracer->flush();
+        }
         return $next($request);
     }
 }
